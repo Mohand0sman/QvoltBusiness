0df0d0e6634502459b87228c7cdb8a31c31434cf
MOHAND OSMAN <mohand.osman.adam@gmail.com>
2025-04-30 15:31:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const track = document.querySelector('.brands-track');
+    const logos = track.innerHTML; // نحفظ الصور الأصلية
+    track.innerHTML += logos; // نكررها مرة ثانية
+
+
+
+
+});
+
+function wats() {
+    window.location.href = "";
+}
+
+
